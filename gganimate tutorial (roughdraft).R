14917acc7dbc3cb581486dbd89e6c71e269ab658
Dufus89 <123115630+Dufus89@users.noreply.github.com>
2023-11-01 11:57:14 -0400

    Add files via upload

---
+++ gganimate tutorial (roughdraft).R
@@ -0,0 +1,7 @@
+#install gganimate
+install.packages("gganimate")
+#load ggplot2 and gganimate
+library("ggplot2")
+library("gganimate")
+#load example data titled "Snake perch leaving"
+Perch <- read.csv("Snake perch leaving.csv")
