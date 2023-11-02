a8ef40a8d1ce9614a60b293850fa0ef24667ea1e
Dufus89 <123115630+Dufus89@users.noreply.github.com>
2023-11-02 12:18:59 -0400

    Update gganimate tutorial (roughdraft).R

--- gganimate tutorial (roughdraft).R
+++ gganimate tutorial (roughdraft).R
@@ -4,7 +4,7 @@ install.packages("gganimate")
 library("ggplot2")
 library("gganimate")
 #load example data titled "Snake perch leaving"
-Perch <- read.csv("Snake perch leaving.csv")
+Perch <- read.csv("Snake perch leaving.csv", fileEncoding="UTF-8-BOM")
 #Lets a create a plot that shows the number of snakes of each species that left the perch at each of the height and target conditions.
 plot1 <- ggplot(Perch, aes(x = Target,
                           y = Left.the.perch,
@@ -12,7 +12,6 @@ plot1 <- ggplot(Perch, aes(x = Target,
   geom_bar(position="dodge", stat="identity",colour="black") + 
   facet_wrap(~Height)
 plot1
-gg_animate(plot1)
 #Lets add in an transition animation based on species
 plot1 + transition_states(
   Species,
